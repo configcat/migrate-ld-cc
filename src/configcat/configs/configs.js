555b3969780a1b7f630f6b6df58b8175362be69a
Kiong <kiong90@gmail.com>
2021-11-15 10:08:23 +0800

    feat: add config cat config class

---
+++ src/configcat/configs/configs.js
@@ -0,0 +1,59 @@
+import { ConfigCat } from '../config-cat.js';
+
+export class ConfigCatConfigs extends ConfigCat {
+  async create(productId, name, description='') {
+    try {
+      const opts = {
+        url: `${this.BASE_URL}/v1/products/${productId}/configs`,
+        body: JSON.stringify({
+          name,
+          description
+        })
+      };
+
+      const res = super.post(opts);
+
+      return res;
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async getAll(productId) {
+    try {
+      const opts = {
+        url: `${this.BASE_URL}/v1/products/${productId}/configs`
+      };
+
+      const res = super.gets(opts);
+
+      return res;
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  transform(data={}) {
+    return data?.map((item) => ({
+      configId: item.configId,
+      name: item.name,
+      description: item.description
+    }));
+
+    // .map(item => {
+    //     const envs = {}
+    //     for (const [key, value] of Object.entries(object1)) {
+    //       envs[key] = 
+    //     }
+
+
+    //     const envs = Object.entries(item?.environments || {})?.map(entry => {
+    //       const key =
+    //     })
+    //   }(
+    //     envs: item?.environments?.map(env => ({m
+    //       name: 
+    //     }))
+    //   ))
+  }
+}
