c512b46769379db81b94a1a988cc78fa6aa43b77
Alastair Lockie <alastair@brandwatch.com>
2023-10-24 13:53:34 +0100

    Add additional debugging for configcat requests

--- src/configcat/config-cat.js
+++ src/configcat/config-cat.js
@@ -1,5 +1,8 @@
 import { camelCase } from 'camel-case';
+import NodeFetch from 'node-fetch';
 import { BaseApi } from '../utils/base-api.js';
+
+let noOfRequests = 0;
 
 export class ConfigCat extends BaseApi {
     BASE_URL = 'https://api.configcat.com';
@@ -11,7 +14,23 @@ export class ConfigCat extends BaseApi {
         pricing_plan = process.env.CONFIG_CAT_PLAN,
         camelcase = camelCase
     ) {
-        super();
+        super(async function (...args) {
+            noOfRequests++;
+            const resp = await NodeFetch(...args);
+            console.log(
+                `Number of requests: ${noOfRequests} with ${resp.headers.get(
+                    'x-rate-limit-remaining'
+                )} remaining`
+            );
+            if (!resp.ok) {
+                console.log(resp.headers);
+                console.dir({
+                    remainingRequests: resp.headers.get('x-rate-limit-remaining'),
+                    limitReset: resp.headers.get('x-rate-limit-reset'),
+                });
+            }
+            return resp;
+        });
         this.HEADERS = {
             Authorization: apiKey,
         };
